af4d66a7917a0a7306d3593ca8b0004ea0e15a00
Max Müller <muellerprints@t-online.de>
2025-11-03 09:01:28 +0100

    update

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,52 +6,58 @@ const Footer = () => {
 			<div className="container">
 				<div className="centered">
 					<div className="row">
-						<div className="col-lg-4 col-md-4 col-sm-4">
+						{/* Branding/Adresse links */}
+						<div className="col-lg-3 col-md-3 col-sm-6">
 							<div className="footer_dv">
 								<br />
-								<h4>Kontakt</h4>
+								<strong>MUELLERPRINTS.<br />STUTTGART</strong>
 								<ul>
 									<li />
 									<li>T + 49 (0)711 / 262 49 64</li>
-									<li>
-										<a href="mailto:muellerprints@t-online.de">muellerprints@t-online.de</a>
-									</li>
-									<li>
-										<a href="/office-hours">Öffnungszeiten</a>
-									</li>
+									<li>Mo – Do 9.00 - 16.00 Uhr<br />Fr 9.00 - 14 Uhr</li>
 								</ul>
 							</div>
 						</div>
-						<div className="col-lg-4 col-md-4 col-sm-4">
+
+						{/* Kontakt */}
+						<div className="col-lg-3 col-md-3 col-sm-6">
 							<div className="footer_dv">
 								<br />
-								<h4>Rechtliches</h4>
+								<h4>KONTAKT</h4>
 								<ul>
 									<li />
-									<li>
-										<a href="/imprint">Impressum</a>
-									</li>
-									<li>
-										<a href="/disclaimer">Datenschutz</a>
-									</li>
-									<li>
-										<a href="/payment">Zahlungsarten</a>
-									</li>
+									<li><a href="/office-hours">Öffnungszeiten</a></li>
+									<li><a href="/anfahrt">Anfahrt</a></li>
+									<li><a href="/imprint">Impressum</a></li>
 								</ul>
 							</div>
 						</div>
-						<div className="col-lg-4 col-md-4 col-sm-4">
+
+						{/* About */}
+						<div className="col-lg-3 col-md-3 col-sm-6">
 							<div className="footer_dv">
 								<br />
-								<h4>Sonstiges</h4>
+								<h4>ABOUT</h4>
 								<ul>
 									<li />
-									<li>
-										<a href="/links">Links</a>
-									</li>
-									<li>
-										<a href="/anfahrt">Anfahrt</a>
-									</li>
+									<li><a href="/bestellverlauf">Bestellverlauf</a></li>
+									<li><a href="/versandarten">Versandarten</a></li>
+									<li><a href="/payment">Zahlungsarten</a></li>
+								</ul>
+							</div>
+						</div>
+
+						{/* Rechtliches */}
+						<div className="col-lg-3 col-md-3 col-sm-6">
+							<div className="footer_dv">
+								<br />
+								<h4>RECHTLICHES</h4>
+								<ul>
+									<li />
+									<li><a href="/disclaimer">Datenschutzerklärung</a></li>
+									<li><a href="/cookies">Verwendung von Cookies</a></li>
+									<li><a href="/agbs">Allgemeine Geschäftsbedingungen</a></li>
+									<li><a href="/sitemap.xml">Sitemap</a></li>
 								</ul>
 							</div>
 						</div>
